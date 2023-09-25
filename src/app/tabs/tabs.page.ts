372260e1e1dd4fb9492230c2e8a23f5effbf2ba9
Trần Tuyên <20130459@st.hcmuaf.edu.vn>
2023-09-25 12:19:05 +0700

    thêm menu danh mục

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -3,9 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  styleUrls: ['tabs.page.scss'],
 })
 export class TabsPage {
+
+  isNikeMenuOpen = false;
+  isAdidasMenuOpen = false;
+  isJordanMenuOpen = false;
 
   constructor() {}
 
